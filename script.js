a627fa1996c94ab6ec48a25e39a776ff57b20475
Stefan Countryman <stefan.countryman@gmail.com>
2016-06-11 21:13:02 -0400

    start adding flying emoji

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-COLOR_CHANGE_INTERVAL = 100
+var COLOR_CHANGE_INTERVAL = 100
+var COLORS = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+// change colors
 var h1 = document.getElementsByTagName("h1");
 var h2 = document.getElementsByTagName("h2");
-var colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 function getDifferentRandomColor(text) {
-  var i = colors.indexOf(text.style.color);
-  i = (i + 1 + Math.floor(Math.random() * (colors.length - 1))) % colors.length;
-  return colors[i];
+  var i = COLORS.indexOf(text.style.color);
+  i = (i + 1 + Math.floor(Math.random() * (COLORS.length - 1))) % COLORS.length;
+  return COLORS[i];
 }
 function randomColorize(headings) {
   for (var i=0; i<headings.length; i++) {
@@ -17,3 +19,6 @@ function randomlyColorizeHeadings() {
   randomColorize(h2);
 }
 window.setInterval(randomlyColorizeHeadings, COLOR_CHANGE_INTERVAL);
+
+// make emoji fly around
+
