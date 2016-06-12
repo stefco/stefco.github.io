b6506a9c8c4a8a4c87eaf3ef31ea4cc6683d2837
Stefan Countryman <stefan.countryman@gmail.com>
2016-06-11 20:54:36 -0400

    make sure color always changes

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
 COLOR_CHANGE_INTERVAL = 100
 var h1 = document.getElementsByTagName("h1");
 var h2 = document.getElementsByTagName("h2");
-function colorize(headers, color) {
-  for (var i=0; i<headers.length; i++) {
-    headers[i].style.color = color
-  }
-}
 var colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
-var i = 0;
-function getRandomColor() {
-  // this avoids color repeats
+function getDifferentRandomColor(text) {
+  var i = colors.indexOf(text.style.color);
   i = (i + 1 + Math.floor(Math.random() * (colors.length - 1))) % colors.length;
   return colors[i];
 }
-function randomColorize(headers) {
-  for (var i=0; i<headers.length; i++) {
-    headers[i].style.color = getRandomColor();
+function randomColorize(headings) {
+  for (var i=0; i<headings.length; i++) {
+    headings[i].style.color = getDifferentRandomColor(headings[i]);
   }
 }
-function randomlyColorizeHeaders() {
+function randomlyColorizeHeadings() {
   randomColorize(h1);
   randomColorize(h2);
 }
-window.setInterval(randomlyColorizeHeaders, COLOR_CHANGE_INTERVAL);
+window.setInterval(randomlyColorizeHeadings, COLOR_CHANGE_INTERVAL);
